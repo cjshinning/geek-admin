37c11ac7e4d1b0c8ac47f6d633c3dc59f02ad8e4
chenjie3 <chenjie3@37.com>
2022-11-23 17:43:21 +0800

    基础入门

---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>这是首页</h1>
+    <TodoList/>
+    <Rate :value="3"/>
+    <Rate :value="4" theme="red"/>
+    <Rate :value="1" theme="green"/>
+</template>
+<script setup>
+import { ref } from 'vue';
+import TodoList from '../components/Todolist.vue';
+import Rate from '../components/Rate.vue';
+let score = ref(3);
+</script>
