52776f66157bfe5a1b1eb8966a5672e6a3bfeb55
chenjie3 <chenjie3@37.com>
2022-11-23 17:57:05 +0800

    实战篇1

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,10 +4,12 @@
     <Rate :value="3"/>
     <Rate :value="4" theme="red"/>
     <Rate :value="1" theme="green"/>
+    <Count/>
 </template>
 <script setup>
 import { ref } from 'vue';
 import TodoList from '../components/Todolist.vue';
 import Rate from '../components/Rate.vue';
+import Count from '../components/Count.vue';
 let score = ref(3);
 </script>
