54c7fa16798bf169d69c4af05b1707f96da1de10
chenjie3 <chenjie3@37.com>
2022-11-24 09:43:48 +0800

    JSX使用

---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 v-if="num==1">标题{{num}}</h1>
+  <h2 v-if="num==2">标题{{num}}</h2>
+  <h3 v-if="num==3">标题{{num}}</h3>
+  <h4 v-if="num==4">标题{{num}}</h4>
+  <h5 v-if="num==5">标题{{num}}</h5>
+  <h6 v-if="num==6">标题{{num}}</h6>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+    num: {
+        type: Number,
+        required: true
+    }
+})
+
+console.log(props.num) // [1,2,3,4,5]
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,13 @@
 <template>
-    <h1>这是首页</h1>
-    <TodoList/>
-    <Rate :value="3"/>
-    <Rate :value="4" theme="red"/>
-    <Rate :value="1" theme="green"/>
-    <Count/>
+  <h1>这是首页</h1>
+  <TodoList />
+  <!-- <Heading num="1"/> -->
+  <Heading :level="2">hello geekbang</Heading>
 </template>
 <script setup>
-import { ref } from 'vue';
-import TodoList from '../components/Todolist.vue';
-import Rate from '../components/Rate.vue';
-import Count from '../components/Count.vue';
-let score = ref(3);
+import {ref} from 'vue';
+// import TodoList from "../components/Todolist.vue";
+import TodoList from "../components/Todolist.jsx";
+// import Heading from "../components/Heading.vue";
+import Heading from "../components/Heading.jsx";
 </script>
